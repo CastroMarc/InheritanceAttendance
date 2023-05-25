040d4a886f241de133082a13504dfd7208910fac
Marc Luiz Castro <111546344+CastroMarc@users.noreply.github.com>
2023-05-25 10:03:42 +0800

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,23 @@
+
+public class Vehicle {
+	
+	//TOYOTA VIOS
+	String CarSpeed = "70 kph";
+	String CarColor = "White";
+	int CarPrice = 1105000;
+
+	//U-2 Spy Plane
+	String PlaneSpeed = "250 kph";
+	String PlaneColor = "Blue";
+	int PlanePrice = 35105000;
+	
+	//Fandango Yacht
+	String BoatSpeed = "30 kph";
+	String BoatColor = "green";
+	int BoatPrice = 6015000;
+	
+	void STOP() {
+		System.out.println ("ITIGIL NA NATIN TOH");
+	}
+
+}
